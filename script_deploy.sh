6c20958133b4ef3f8dae3d5d2aeba558a57000fb
Lucas Beninca <lucasbeninca@proton.me>
2025-08-23 16:02:24 -0300

    Update script_deploy.sh

--- script_deploy.sh
+++ script_deploy.sh
@@ -1,19 +1,24 @@
 #! /bin/bash
 
-docker load -i vollmed-api.tar
+# Descompacta a imagem Docker onde está a build da API
+docker load -i vollmed-test-sonarcloud_api.tar
 
+# Renomeia o Docker compose de produção
 mv docker-compose-prod.yaml docker-compose.yaml
 
+# Listar os IDs dos containers em execução
 container_ids=$(docker ps -q)
 
-if [ -z "$conteiner_ids" ]; then
-  echo "Não há containers em execução"
+# Verificar se há containers em execução
+if [ -z "$container_ids" ]; then
+    echo "Não há containers em execução."
 else
-  for container_id in $container_ids; do
-    echo "Parando container: $container_id"
-    docker stop $container_id
-  done
-  echo "Todos os containers em execução foram parados."
+for container_id in $container_ids; do
+            echo "Parando container: $container_id"
+            docker stop $container_id
+done
+echo "Todos os containers em execução foram parados."
 fi
 
+# Sobe os containers da aplicação
 docker compose up -d
